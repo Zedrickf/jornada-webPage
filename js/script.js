a9bd4ead0f21a27ba2e3053a1fc5d03d80ddfece
Zedrickf <giullios.fitaszdk@gmail.com>
2024-09-22 17:10:41 -0500

    create and add the JS file. Add styles to screen shorten than 768px. Add icon footer

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+//imprime un mensaje de alerta cuando se presiona alguna de los botones
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.getElementsByClassName('myButton');  // Selecciona todos los botones con la clase 'myButton'
+
+    // Recorre todos los botones y añade un event listener a cada uno
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            alert('Trabajo en progreso');
+        });
+    }
+});
+
+//muestra las opciones en el banner 
+document.addEventListener("DOMContentLoaded", function() {
+    const menuBtn = document.querySelector(".mainHeader__menuBtn");
+    const nav = document.querySelector(".mainHeader__nav");
+
+    menuBtn.addEventListener("click", function() {
+        nav.classList.toggle("open");
+        menuBtn.classList.toggle("open"); // Esto aplica la transformación de las líneas
+    });
+});
